05c1d2b33d4c3f779ddd26777268415a72639540
nayannelbatista <nayannelopes.bd@gmail.com>
2024-10-21 16:29:38 -0300

    Utilizando isPlatformBrowser e isPlatformServer e adicionando lógica para listar ofertas de acordo com a localização

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, inject, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { MatGridList, MatGridTile } from '@angular/material/grid-list';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser, isPlatformServer } from '@angular/common';
 import { MatList, MatListItem } from '@angular/material/list';
 import { Router } from '@angular/router';
 import { ProductService } from '../../services/product.service';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-products-list',
@@ -20,21 +21,78 @@ import { ProductService } from '../../services/product.service';
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.css'
 })
-export class ProductsListComponent {
+export class ProductsListComponent implements OnInit {
 
-  productsByCategory: { category: string, products: any[] }[] = [];
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  offers: string[] = [];
+  productsByCategory: { category: string, products: Product[] }[] = [];
+  //private productService = inject(ProductService) outra forma de DI
 
-  constructor(private productService: ProductService, private router: Router) {
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: object) {
+    afterNextRender(() => {
+      this.getCurrentLocation();
+    });
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.getCurrentLocation();
+    }
+
+    if (isPlatformServer(this.platformId)) {
+      console.log('Este código está sendo executado no servidor.');
+    }
     const products = this.productService.getProducts();
     this.groupProductsByCategory(products);
   }
 
-  groupProductsByCategory(products: any[]) {
+  groupProductsByCategory(products: Product[]) {
     const categories = [...new Set(products.map(product => product.category))];
     this.productsByCategory = categories.map(category => ({
       category,
       products: products.filter(product => product.category === category)
     }));
+  }
+
+  private getCurrentLocation(): void {
+    try {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            console.log('Latitude:', position.coords.latitude);
+
+            this.getOffersBasedOnLocation(position.coords.latitude);
+          },
+          error => {
+            console.error('Erro ao obter geolocalização:', error);
+          }
+        );
+      } else {
+        console.error('Geolocalização não suportada no navegador.');
+      }
+    } catch (error) {
+      console.error('Erro ao acessar a geolocalização no servidor:', error);
+    }
+  }
+
+  private getOffersBasedOnLocation(latitude: number): void {
+    this.filteredProducts = this.products.filter(product => {
+      return latitude > 0 && product.category === 'Milkshakes';
+    });
+
+    if (latitude < -23) {
+      this.offers = ['Oferta para a região Sul: 20% de desconto no milkshake de morango'];
+    } else if (latitude < -10) {
+      this.offers = ['Oferta para a região Sudeste: 15% de desconto no milkshake de frutas verdes'];
+    } else if (latitude >= -10 && latitude < 0) {
+      this.offers = ['Oferta para a região Nordeste: 25% de desconto no milkshake de amora'];
+    } else {
+      this.offers = ['Oferta: 10% de desconto no milkshake de limão'];
+    }
   }
 
   goToDetails(productId: number) {
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { afterNextRender, Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ProductsListComponent } from '../../components/products-list/products-list.component';
 
 @Component({
@@ -8,40 +8,4 @@ import { ProductsListComponent } from '../../components/products-list/products-l
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
-  ngOnInit(): void {
-  //   try {
-  //     // Tentando acessar a geolocalização, que não estará disponível no servidor
-  //     navigator.geolocation.getCurrentPosition(position => {
-  //       console.log('Latitude:', position.coords.latitude);
-  //     });
-  //   } catch (error) {
-  //     console.error('Erro ao acessar a geolocalização no servidor:', error);
-  //   }
-  }
-
-  // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/geolocation
-  //https://v17.angular.io/guide/lifecycle-hooks
-
-  constructor() {
-    afterNextRender(() => {
-      try {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(position => {
-            console.log('Latitude:', position.coords.latitude);
-            console.log('Precisão:', position.coords.accuracy);
-            console.log('Timestamp:', position.timestamp);
-          });
-        } else {
-          console.error('Geolocalização não suportada no navegador.');
-        }
-      } catch (error) {
-        console.error('Erro ao acessar a geolocalização no servidor:', error);
-      }
-    });
-  }
-
-  
-
-  
-}
+export class HomeComponent {}
