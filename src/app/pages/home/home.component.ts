abc12dc0e5d07883252580e00c66875b46690c94
nayannelbatista <nayannelopes.bd@gmail.com>
2024-10-22 15:43:01 -0300

    Adicionando metatags na home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ProductsListComponent } from '../../components/products-list/products-list.component';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,23 @@ import { ProductsListComponent } from '../../components/products-list/products-l
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit{
+  private readonly title = inject(Title);
+  private readonly meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.setPageMeta();
+  }
+
+  setPageMeta(): void {
+    this.title.setTitle('Deleite - a melhor experiência de sabores!');
+    this.meta.addTags([
+      { name: 'description', content: 'Descubra os melhores milkshakes, sorvetes e smoothies na Deleite. Sabor e qualidade em cada produto!' },
+      { property: 'og:title', content: 'Deleite - a melhor experiência de sabores!' },
+      { property: 'og:description', content: 'Descubra os melhores milkshakes, sorvetes e smoothies na Deleite. Sabor e qualidade em cada produto!' },
+      { property: 'og:image', content: 'assets/images/logo.png' },
+      { name: 'twitter:card', content: 'summary_large_image' }
+    ]);
+  }
+  
+}
